522b80a4885b3e13d4ba4d0acf136f900b23e2b7
mh180577 <hugo.millot@etu.univ-lehavre.fr>
2022-12-10 14:56:36 +0100

    Ajout bouton pour directement mettre le code

--- src/CheckTweets.ts
+++ src/CheckTweets.ts
@@ -1,7 +1,7 @@
 import { uri,twitterBearerToken } from "./env.json"
 import { TwitterApi } from 'twitter-api-v2';
 import { MongoClient } from "mongodb";
-import { Client,EmbedBuilder,TextChannel } from "discord.js";
+import { Client,EmbedBuilder,TextChannel,ActionRowBuilder,ButtonBuilder,ButtonStyle } from "discord.js";
 
 export async function checkTweets(clientDiscord:Client){
     const clientMongo = new MongoClient(uri);
@@ -20,14 +20,23 @@ export async function checkTweets(clientDiscord:Client){
         await lastTweetCollection.updateOne({},{$set:{tweetId:lastTweetUberEats.id}})
         const guildInfo = database.collection('guildinfo');
         const guilds = await (await guildInfo.find()).toArray();
+        const row = new ActionRowBuilder<ButtonBuilder>()
+			.addComponents(
+				new ButtonBuilder()
+					.setLabel('Redeem')
+					.setStyle(ButtonStyle.Link)
+                    .setURL("https://www.ubereats.com/ch/feed?diningMode=DELIVERY&mod=promos"),
+			);
+
         const embed = new EmbedBuilder().setAuthor({name:"Uber Eats France (@ubereats_fr)",iconURL:"https://pbs.twimg.com/profile_images/1526507008505126912/KLpm9_UY_400x400.jpg",url:"https://twitter.com/ubereats_fr"}).setColor("#00c75f").setDescription(lastTweetUberEats.text).setFooter({text:"Twitter",iconURL:"https://cdn.cms-twdigitalassets.com/content/dam/developer-twitter/images/Twitter_logo_blue_48.png"}).setTimestamp(new Date(lastTweetUberEats?.created_at || Date.now()));
+        
         for(let guild of guilds){
             if(guild.channelId && guild.roleId){
                 clientDiscord.guilds.fetch(guild.guildId).then(discordGuild => {
                     discordGuild.channels.fetch(guild.channelId).then((channel) => {
                         if(channel?.isTextBased()){
                             let textChan:TextChannel = (channel as TextChannel);
-                            textChan.send({ embeds:[embed] })
+                            textChan.send({ embeds:[embed],components:[row] })
                             textChan.send({ content:guild.roleId ? `<@&${guild.roleId}>` : "New Uber Eat Tweets" })
                         }
                     })
